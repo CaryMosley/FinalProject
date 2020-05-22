726202839a64b69627c314787877920f826878f8
CaryMosley <57460111+CaryMosley@users.noreply.github.com>
2020-05-22 15:42:26 -0400

    initial LSTM

--- Notebooks/timeseriesfunctions.py
+++ Notebooks/timeseriesfunctions.py
@@ -229,7 +229,37 @@ def plot_ARIMAX_forecasts(time_series_diff,time_series, AR, MA, exogenous):
     plt.title('Forecast vs Actuals')
     plt.legend(loc='upper left', fontsize=8)
     plt.show()    
-    
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+    """This function came from Jason Brownlee of Machine Learning Mastery.com
+    Frame a time series as a supervised learning dataset. Arguments:
+    mdata: Sequence of observations as a list or NumPy array.
+    n_in: Number of lag observations as input (X).
+    n_out: Number of observations as output (y).
+    dropnan: Boolean whether or not to drop rows with NaN values.Returns:
+    Pandas DataFrame of series framed for supervised learning."""
+    n_vars = 1 if type(data) is list else data.shape[1]
+    df = pd.DataFrame(data)
+    cols, names = list(), list()
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+        names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+        if i == 0:
+            names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+        else:
+            names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+    # put it all together
+    agg = pd.concat(cols, axis=1)
+    agg.columns = names
+    # drop rows with NaN values
+    if dropnan:
+        agg.dropna(inplace=True)
+    return agg    
+
+
 def VAR_models_single(time_series,AR_terms,exogenous):
     '''This function takes in multiple timeseries and a set of AR terms to try. It also takes in exogenous variables. The time series
     that it takes in needs to be already stationary. The function then returns a data frame using one exogenous variable at a time
